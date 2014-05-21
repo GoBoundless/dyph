6a08718f884980f7606a2aedc85b2b2998065dbd
Kevin Mook <kevin@kevinmook.com>
2014-05-21 16:17:53 -0400

    Differ's merge_text's body result now is a single string instead of an array. Also add a 'include_base' option that, if set to false, will exclude the base in conflicts

--- spec/lib/dyph3/differ_spec.rb
+++ spec/lib/dyph3/differ_spec.rb
@@ -61,28 +61,43 @@ TEXT
   r.strip
 }
 
-  it "should be tested" do
-    result = Dyph3::Differ.merge_text(left, base, right, markers: {left: "<<<<<<< start", base: "|||||||", right: "=======", close: ">>>>>>> changed_b"})
-    expect(result[:conflict]).to eq(1)
-    expect(result[:body].join("\n")).to eq expected_result
-    
-  end
-
   it "should not explode" do
     result = Dyph3::Differ.merge_text(left, base, right, markers: {left: "<<<<<<< start", base: "|||||||", right: "=======", close: ">>>>>>> changed_b"})
     expect(result[:conflict]).to eq(1)
-    expect(result[:body].join("\n")).to eq expected_result
+    expect(result[:body]).to eq expected_result
   end
 
   it "should not be conflicted when not conflicted" do
     result = Dyph3::Differ.merge_text(left, base, left, markers: {left: "<<<<<<< start", base: "|||||||", right: "=======", close: ">>>>>>> changed_b"})
-    expect(result[:body].join("\n")).to eq left.strip #BUGBUG: differ losing a new line?
+    expect(result[:body]).to eq left.strip #BUGBUG: differ losing a new line?
     expect(result[:conflict]).to eq(0)
   end
 
   it "should not be conflicted when not conflicted" do
     result = Dyph3::Differ.merge_text(base, base, base, markers: {left: "<<<<<<< start", base: "|||||||", right: "=======", close: ">>>>>>> changed_b"})
-    expect(result[:body].join("\n")).to eq base.strip #BUGBUG: differ losing a new line?
+    expect(result[:body]).to eq base.strip #BUGBUG: differ losing a new line?
     expect(result[:conflict]).to eq(0)
+  end
+  
+  it "should allow you to not include the base in the result" do
+    expected_result = <<-TEXT.strip
+This is the baseline.
+<<<<<<< start
+The start (changed by A).
+=======
+The start.
+B added this line.
+>>>>>>> changed_b
+The end.
+cats
+dogs
+pigs
+cows
+chickens
+TEXT
+    
+    result = Dyph3::Differ.merge_text(left, base, right, include_base: false, markers: {left: "<<<<<<< start", base: "|||||||", right: "=======", close: ">>>>>>> changed_b"})
+    expect(result[:conflict]).to eq(1)
+    expect(result[:body]).to eq expected_result
   end
 end
