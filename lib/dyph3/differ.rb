8338aef507a370f63ef3e98ebe5a09aa35e9a40d
Kevin Mook <kevin@kevinmook.com>
2014-05-21 14:57:07 -0400

    fix the diff algorithm. Accidentally inverted the meanings of '..' and '...'

--- lib/dyph3/differ.rb
+++ lib/dyph3/differ.rb
@@ -25,7 +25,7 @@ module Dyph3
     #        (cmd, loA, hiA, loB, hiB), where cmd is either one of
     #        '0', '1', '2', or 'A'.
     def self.diff3(yourtext, origtext, theirtext)
-      # diff result => [(cmd, loA, hiA, loB, hiB), ...]
+      # diff result => [(cmd, loA, hiA, loB, hiB), ..]
       d2 = {
         your: diff(origtext, yourtext),
         their: diff(origtext, theirtext)
@@ -33,12 +33,12 @@ module Dyph3
       d3 = []
       r3 = [nil,  0, 0,  0, 0,  0, 0]
       
-      while d2[:your].length >0 || d2[:their].length > 0
-        # find a continual range in origtext lo2..hi2
+      while d2[:your].length > 0 || d2[:their].length > 0
+        # find a continual range in origtext lo2...hi2
         # changed by yourtext or by theirtext.
         #
         #     d2[:your]            222    222222222
-        #  origtext             ...L!!!!!!!!!!!!!!!!!!!!H...
+        #  origtext             ..L!!!!!!!!!!!!!!!!!!!!H..
         #     d2[:their]             222222   22  2222222
         
         i_target = nil
@@ -82,14 +82,14 @@ module Dyph3
         lo2 = r2[i_target][ 0][1]
         hi2 = r2[j_target][-1][2]
         
-        # take the corresponding ranges in yourtext lo0..hi0
-        # and in theirtext lo1..hi1.
+        # take the corresponding ranges in yourtext lo0...hi0
+        # and in theirtext lo1...hi1.
         #
-        #   yourtext     ..L!!!!!!!!!!!!!!!!!!!!!!!!!!!!H...
+        #   yourtext     ...L!!!!!!!!!!!!!!!!!!!!!!!!!!!!H..
         #   d2[:your]       222    222222222
-        #   origtext     ...00!1111!000!!00!111111...
+        #   origtext     ..00!1111!000!!00!111111..
         #   d2[:their]        222222   22  2222222
-        #  theirtext          ...L!!!!!!!!!!!!!!!!H...
+        #  theirtext          ..L!!!!!!!!!!!!!!!!H..
         if r2[:your]
           lo0 = r2[:your][ 0][3] - r2[:your][ 0][1] + lo2
           hi0 = r2[:your][-1][4] - r2[:your][-1][2] + hi2
@@ -106,15 +106,15 @@ module Dyph3
         end
         
         # detect type of changes
-        if !r2[:your]
+        if r2[:your].length == 0
           cmd = '1'
-        elsif not r2[:their]
+        elsif r2[:their].length == 0
           cmd = '0'
         elsif hi0 - lo0 != hi1 - lo1
           cmd = 'A'
         else
           cmd = '2'
-          (0 ... hi0 - lo0).each do |d|
+          (0 .. hi0 - lo0).each do |d|
             (i0, i1) = [lo0 + d - 1, lo1 + d - 1]
             ok0 = (0 <= i0 && i0 < yourtext.length)
             ok1 = (0 <= i1 && i1 < theirtext.length)
@@ -133,19 +133,20 @@ module Dyph3
     def self.merge(yourtext, origtext, theirtext)
       res = {conflict: 0, body: []}
       d3 = diff3(yourtext, origtext, theirtext)
+      
       text3 = [yourtext, theirtext, origtext]
       i2 = 1
       d3.each do |r3|
-        (i2 .. r3[5]).each do |lineno|                  # exclusive (..)
+        (i2 ... r3[5]).each do |lineno|                  # exclusive (...)
           res[:body] << text3[2][lineno - 1]
         end
         
         if r3[0] == '0'
-          (r3[1] ... r3[2]).each do |lineno|            # inclusive (...)
+          (r3[1] .. r3[2]).each do |lineno|            # inclusive (..)
             res[:body] << text3[0][lineno - 1]
           end
         elsif r3[0] != 'A'
-          (r3[3] ... r3[4]).each do |lineno|            # inclusive (...)
+          (r3[3] .. r3[4]).each do |lineno|            # inclusive (..)
             res[:body] << text3[1][lineno - 1]
           end
         else
@@ -154,7 +155,7 @@ module Dyph3
         i2 = r3[6] + 1
       end
       
-      (i2 ... text3[2].length).each do |lineno|         # inclusive (...)
+      (i2 .. text3[2].length).each do |lineno|         # inclusive (..)
         res[:body] << text3[2][lineno - 1]
       end
       
@@ -169,7 +170,7 @@ module Dyph3
       d    = []
       uniq = [[text_a.length, text_b.length]]
       
-      (freq, ap, bp) = [{}, {}, {}]
+      freq, ap, bp = [{}, {}, {}]
       text_a.each_with_index do |s, i|
         freq[s] ||= 0
         freq[s] += 2
@@ -186,9 +187,9 @@ module Dyph3
         end
       end
       
-      (freq, ap, bp) = [{}, {}, {}]
+      freq, ap, bp = [{}, {}, {}]
       uniq.sort!{|a, b| a[0] <=> b[0]}
-      (a1, b1) = [0, 0]
+      a1, b1 = [0, 0]
       while a1 < text_a.length && b1 < text_b.length
         if text_a[a1] != text_b[b1]
           break
@@ -241,26 +242,26 @@ module Dyph3
       
       def self._conflict_range(text3, r3, res)
         text_a = [] # their text
-        (r3[3] ... r3[4]).each do |i|                   # inclusive(...)
+        (r3[3] .. r3[4]).each do |i|                   # inclusive(..)
           text_a << text3[1][i - 1]
         end
         text_b = [] # your text
-        (r3[1] ... r3[2]).each do |i|                   # inclusive(...)
+        (r3[1] .. r3[2]).each do |i|                   # inclusive(..)
           text_b << text3[0][i - 1]
         end
         d = diff(text_a, text_b)
-        if _assoc_range(d, 'c') && r3[5] <= r3[6]
+        if !_assoc_range(d, 'c').nil? && r3[5] <= r3[6]
           res[:conflict] += 1
           res[:body] << '<<<<<<<'
-          (r3[1] ... r3[2]).each do |lineno|
+          (r3[1] .. r3[2]).each do |lineno|
             res[:body] << text3[0][lineno - 1]
           end
           res[:body] << '|||||||'
-          (r3[5] ... r3[6]).each do |lineno|
+          (r3[5] .. r3[6]).each do |lineno|
             res[:body] << text3[2][lineno - 1]
           end
           res[:body] << '======='
-          (r3[3] ... r3[4]).each do |lineno|
+          (r3[3] .. r3[4]).each do |lineno|
             res[:body] << text3[1][lineno - 1]
           end
           res[:body] << '>>>>>>>'
@@ -270,29 +271,29 @@ module Dyph3
         ia = 1
         
         d.each do |r2|
-          (ia .. r2[1]).each do |lineno|
+          (ia ... r2[1]).each do |lineno|
             res[:body] << text_a[lineno - 1]
           end
           if r2[0] == 'c'
             res[:conflict] += 1
             res[:body] << '<<<<<<<'
-            (r2[3] ... r2[4]).each do |lineno|
+            (r2[3] .. r2[4]).each do |lineno|
               res[:body] << text_b[lineno - 1]
             end
             res[:body] << '======='
-            (r2[1] ... r2[2]).each do |lineno|
+            (r2[1] .. r2[2]).each do |lineno|
               res[:body] << text_a[lineno - 1]
             end
             res[:body] << '>>>>>>>'
           elsif r2[0] == 'a'
-            (r2[3] ... r2[4]).each do |lineno|
+            (r2[3] .. r2[4]).each do |lineno|
               res[:body] << text_b[lineno - 1]
             end
           end
           ia = r2[2] + 1
         end
         
-        (ia .. text_a.length).each do |lineno|
+        (ia ... text_a.length).each do |lineno|
           res[:body] << text_a[lineno - 1]
         end
         
