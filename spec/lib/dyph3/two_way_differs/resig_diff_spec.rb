ea013edeac6d512ac41c6b768b1a935e6f8eed71
Andrew Montalto <andrew@boundless.com>
2015-02-10 11:36:07 -0500

    clean up heckel test abit, need to fix output

--- spec/lib/dyph3/two_way_differs/resig_diff_spec.rb
+++ spec/lib/dyph3/two_way_differs/resig_diff_spec.rb
@@ -1,25 +1,14 @@
 require 'spec_helper'
 
 describe Dyph3::TwoWayDiffers::ResigDiff do
-  describe "monkey" do
+  describe "" do
     let(:differ) { Dyph3::TwoWayDiffers::ResigDiff }
     describe "complex changes" do
       xit "should find a change" do
         t1 = "No TV and no beer make Homer go crazy".split
         t2 = "No work and much beer make Homer crazy and naked".split
-        t3 = "No TV and wearing clothes make Homer go crazy".split
-        # t1 = "b a d b".split
-        # t2 = "b a c d b".split
-
         d1 = differ.diff(t1,t2)
-        d2 = Dyph3::TwoWayDiffers::HeckelDiff.diff(t1,t2)
-        # binding.pry
-        
-        # pp d1
-        # pp d2
-        x = Dyph3::Differ.merge_text t2.join("\n"), t1.join("\n"), t3.join("\n")
-        # puts x
-        # binding.pry
+        expect(d1).to eq 'test'
       end
     end
   end
