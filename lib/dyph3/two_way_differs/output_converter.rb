9709c0db82afbfa0c5e96b1966c09d8c464c203a
Andrew Montalto <andrew@boundless.com>
2015-02-16 16:41:06 -0500

    refactor a complex method a bit

--- lib/dyph3/two_way_differs/output_converter.rb
+++ lib/dyph3/two_way_differs/output_converter.rb
@@ -2,36 +2,68 @@ module Dyph3
   module TwoWayDiffers
     module OutputConverter
       extend self
+
+      def convert_to_dyph3_output(merged_text)
+        merge_output_lines = merged_text.map { |x| to_output_format x }
+        actions = partition_into_actions(merge_output_lines)
+        collapsed_actions = actions.map { |action| collapse_action(action) }
+        trimmed_result = pair_up_add_deletes collapsed_actions
+        no_changes = trimmed_result.reject { |x| x[:action] == :no_change}
+        t2 = set_offset(no_changes)
+        t2.map { |r| Dyph3::Support::AssignAction.get_action(
+          lo_a: r[:old_lo]-1 ,
+          lo_b: r[:new_lo]-1,
+          hi_a: r[:old_hi]-1,
+          hi_b: r[:new_hi]-1
+        )}
+      end
+
       def merge_results(calling_class, old_text, new_text)
         merged_text = []
 
         if (new_text.empty?)
-          old_text.map.with_index do |line, i|
-            merged_text << { action: :delete, line: line, old_index: i+1, new_index: i}
-          end
+          no_new_text(old_text, merged_text)
         else
-          if optional(old_text.first).text.value.nil?
-            old_text.map.with_index.each do |line, i|
-              break if !optional(line).text.value.nil?
-              merged_text << {action: :delete, line: line, old_index:i+1, new_index: i}
-            end
-          end
-
-          prev_no_change_old = - 1
-          new_text.map.with_index.each do |line, i|
-            if optional(line).text.value.nil?
-              merged_text << {action: :add, line: line, old_index: prev_no_change_old + 1, new_index: i+1}
-            else
-              prev_no_change_old = line.row
-              merged_text << {action: :no_change, line: line, old_index: line.row, new_index:  i}
-              ((prev_no_change_old+1) ... old_text.length).each do |n|
-                break if !optional(old_text[n]).text.value.nil?
-                merged_text << {action: :delete, line: old_text[n], old_index: n+1, new_index: i+1}
-              end
-            end
-          end
+          prepend_old_text(old_text, merged_text)
+          gather_up_actions(old_text, new_text, merged_text)
         end
         merged_text
+      end
+
+      def gather_up_actions(old_text, new_text, merged_text)
+        prev_no_change_old = - 1
+        new_text.map.with_index.each do |line, i|
+          if optional(line).text.value.nil?
+            merged_text << {action: :add, line: line, old_index: prev_no_change_old + 1, new_index: i+1}
+          else
+            prev_no_change_old = line.row
+            change_or_delete(old_text, line, prev_no_change_old, merged_text, i)
+          end
+        end
+      end
+
+      def change_or_delete(old_text, line, prev_no_change_old, merged_text, index)
+        merged_text << {action: :no_change, line: line, old_index: line.row, new_index: index}
+
+        ((prev_no_change_old+1) ... old_text.length).each do |n|
+          break if !optional(old_text[n]).text.value.nil?
+          merged_text << {action: :delete, line: old_text[n], old_index: n+1, new_index: index+1}
+        end
+      end
+
+      def prepend_old_text(old_text, merged_text)
+        if optional(old_text.first).text.value.nil?
+          old_text.map.with_index.each do |line, i|
+            break if !optional(line).text.value.nil?
+            merged_text << {action: :delete, line: line, old_index:i+1, new_index: i}
+          end
+        end
+      end
+
+      def no_new_text(old_text, merged_text)
+        old_text.map.with_index do |line, i|
+          merged_text << { action: :delete, line: line, old_index: i+1, new_index: i}
+        end
       end
 
       def set_offset(results)
@@ -94,21 +126,6 @@ module Dyph3
           end
         end
         results
-      end
-
-      def convert_to_dyph3_output(merged_text)
-        merge_output_lines = merged_text.map { |x| to_output_format x }
-        actions = partition_into_actions(merge_output_lines)
-        collapsed_actions = actions.map { |action| collapse_action(action) }
-        trimmed_result = pair_up_add_deletes collapsed_actions
-        no_changes = trimmed_result.reject { |x| x[:action] == :no_change}
-        t2 = set_offset(no_changes)
-        t2.map { |r| Dyph3::Support::AssignAction.get_action(
-          lo_a: r[:old_lo]-1 ,
-          lo_b: r[:new_lo]-1,
-          hi_a: r[:old_hi]-1,
-          hi_b: r[:new_hi]-1
-        )}
       end
 
       def partition_into_actions(array)
