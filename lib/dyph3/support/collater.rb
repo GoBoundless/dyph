66dc2598362dcadd2792b42181c95cf1d5648a71
Andrew Montalto <andrew@boundless.com>
2015-02-26 13:12:01 -0500

    update tests, fix the collator and diff3 classes

--- lib/dyph3/support/collater.rb
+++ lib/dyph3/support/collater.rb
@@ -4,13 +4,12 @@ module Dyph3
       extend self
       def collate_merge(left, base, right, merge_result)
         if merge_result.empty?
-          # this happens when all texts are ""
-          text = ""
+          # this happens when all texts are empty
           conflict = false
-          final_result = [{type: :non_conflict, text: ""}]
-          [text, conflict, final_result]
+          final_result = [{type: :non_conflict, text: []}]
+          [[], conflict, final_result]
         else
-          merge_result = handle_trailing_newline(left, base, right, merge_result)
+          #merge_result = handle_trailing_newline(left, base, right, merge_result)
           merge_result = merge_non_conflicts(merge_result)
           get_text_conflict_result(base, merge_result)
         end
@@ -36,7 +35,7 @@ module Dyph3
         def merge_non_conflicts(res, i = 0)
           while i < res.length - 1
             if res[i][:type] == :non_conflict && res[i+1][:type] == :non_conflict
-              res[i][:text] += "\n" unless res[i][:text][-1] == "\n"
+              #res[i][:text] += "\n" unless res[i][:text][-1] == "\n"
               res[i][:text] += res[i+1][:text]
               res.delete_at(i+1)
             else
--- spec/lib/dyph3/support/collator_spec.rb
+++ spec/lib/dyph3/support/collator_spec.rb
@@ -2,47 +2,50 @@ require 'spec_helper'
 
 describe Dyph3::Support::Collater do
   let(:collator) { Dyph3::Support::Collater }
+  let(:text)     { "0\n1\n2\n3\n".split(/(\n)/) } 
+  [ lambda { |x| x.to_s } , lambda { |x| x.to_sym }, lambda { |x| Fish.new(x) } ].each do |f|
 
-  describe ".collate_merge" do
-    it "might be empty" do
-      results = ["", false, [{type: :non_conflict, :text=>""}]]
-      expect(collator.collate_merge("", "", "", [])).to eq results
-    end
+    describe ".collate_merge" do
+      it "might be empty" do
+        results =  [[], false, [{type: :non_conflict, :text=>[]} ]]
+        expect(collator.collate_merge(f.call(""), f.call(""), f.call(""), [])).to eq results
+      end
 
-    it "might be have all non_conflicts" do
-      text = "0\n1\n2\n3\n"
-      collate_merge = (0 .. 3).map { |i| { type: :non_conflict, text: "#{i}" }}
-      result = [text, false,
-        [{:type=>:non_conflict, :text=>text}]]
-      expect(collator.collate_merge(text, text, text, collate_merge)).to eq result
-    end
+      it "might be have all non_conflicts" do
+        collate_merge = text.map { |i| { type: :non_conflict, text: ["#{i}"] }}
+        result = [text, false,
+          [{:type=>:non_conflict, :text=>text}]]
+        expect(collator.collate_merge(text, text, text, collate_merge)).to eq result
+      end
 
-    it "might be have all conflicts" do
-      text = "0\n1\n2\n3\n"
-      collate_merge = (0 .. 3).map { |i| { type: :conflicts, text: "#{i}" }}
-      result = [
-        text,
-        true,
-      collate_merge]
-      expect(collator.collate_merge(text, text, text, collate_merge)).to eq result
-    end
+      it "might be have all conflicts" do
+        collate_merge = text.map { |i| { type: :conflicts, text: "#{i}" }}
+        result = [
+          text,
+          true,
+        collate_merge]
+        expect(collator.collate_merge(text, text, text, collate_merge)).to eq result
+      end
 
-    it "might be a mixed bag" do
-      text = "0\n1\n2\n3\n"
-      conflicted_merge = (0 .. 3).map { |i| { type: :conflicts, text: "#{i}" }}
-      non_conflected_merge = (0 .. 3).map { |i| { type: :non_conflict, text: "#{i}" }}
-      merge_me = [conflicted_merge, non_conflected_merge].flatten
-      result = [
-        text,
-        true,
-      [{:type=>:conflicts, :text=>"0"},
-        {:type=>:conflicts, :text=>"1"},
-        {:type=>:conflicts, :text=>"2"},
-        {:type=>:conflicts, :text=>"3"},
-        {:type=>:non_conflict, :text=>"0\n1\n2\n3\n"}
-      ]]
-
-      expect(collator.collate_merge(text, text, text, merge_me)).to eq result
+      it "might be a mixed bag" do
+        conflicted_merge = text.map { |i| { type: :conflicts, text: ["#{i}"] }}
+        non_conflected_merge = text.map { |i| { type: :non_conflict, text: ["#{i}"] }}
+        merge_me = [conflicted_merge, non_conflected_merge].flatten
+        result = [
+          text,
+          true,
+        [{:type=>:conflicts, :text=>["0"]},
+          {:type=>:conflicts, :text=>["\n"]},
+          {:type=>:conflicts, :text=>["1"]},
+          {:type=>:conflicts, :text=>["\n"]},
+          {:type=>:conflicts, :text=>["2"]},
+          {:type=>:conflicts, :text=>["\n"]},
+          {:type=>:conflicts, :text=>["3"]},
+          {:type=>:conflicts, :text=>["\n"]},
+          {:type=>:non_conflict, :text=> text}
+        ]]
+        expect(collator.collate_merge(text, text, text, merge_me)).to eq result
+      end
     end
   end
 end
