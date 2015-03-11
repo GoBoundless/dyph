b327e0ce7b12f07cafe3205a2c95244d963b37c7
Andrew Montalto <andrew@boundless.com>
2015-03-11 13:29:57 -0400

    add an identity test for the new functional params

--- spec/lib/dyph3/differ_spec.rb
+++ spec/lib/dyph3/differ_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 [Dyph3::TwoWayDiffers::ResigDiff, Dyph3::TwoWayDiffers::HeckelDiff].each do |current_differ|
   describe Dyph3::Differ do
-
     if current_differ == Dyph3::TwoWayDiffers::HeckelDiff
       describe ".merge_two_way_diff" do
         it "show all no changes" do
@@ -31,6 +30,21 @@ require 'spec_helper'
           diff = Dyph3::Differ.merge_two_way_diff(t1, t2)
           expect(diff.map(&:class)).to eq [Dyph3::NoChange, Dyph3::NoChange, Dyph3::Delete, Dyph3::Add, Dyph3::NoChange]
         end
+      end
+    end
+
+    describe "test split" do
+      let(:base) { [:a, :b, :c] }
+      let(:left) { [:a, :b, :c] }
+      let(:right) { [:a, :v, :c] }
+      let(:identity) { ->(x){ x } }
+
+      let(:merged_array) do
+         Dyph3::Differ.merge_text(left, base, right, current_differ: current_differ, split_function: identity, join_funtion: identity )
+      end
+
+      it "should have merged successuffly" do
+        expect(merged_array[0]).to eq right
       end
     end
 
