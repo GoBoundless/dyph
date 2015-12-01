a0c7c2de347d9ae12bfbf0f10b32fb1e7144295b
Andrew Montalto <aamontal@gmail.com>
2015-12-01 18:25:02 -0500

    refactor a few methods to not be while loops

--- lib/dyph3/two_way_differs/heckel_diff.rb
+++ lib/dyph3/two_way_differs/heckel_diff.rb
@@ -1,5 +1,6 @@
 module Dyph3
   module TwoWayDiffers
+
     class HeckelDiff
       # Algorithm adapted from http://www.rad.upenn.edu/sbia/software/basis/apidoc/v1.2/diff3_8py_source.html
 
@@ -16,70 +17,79 @@ module Dyph3
       # @param [in] text_a Array of lines of first text.
       # @param [in] text_b Array of lines of second text.
       # @returns TODO
-      def self.diff(text_a, text_b)
-        d    = []
-        uniq = set_freq(text_a, text_b)
-        uniq.sort!{|a, b| a[0] <=> b[0]}    # sort by the line in which the line was found in a
-        a1, b1 = [0, 0]
-        # set a1 and b1 to be the first line where there is a change.
-        a1, b1 = find_next_difference(text_a, text_b, a1, b1)
-        # start with a1, b1 being the lines before the first change.
-        # for each pair of lines in uniq which definitely match eachother:
-        uniq.each do |a_uniq, b_uniq|
-          # (a_uniq < a1 || b_uniq < b1) == true guarentees there is not a change (since we walked a1 and b1 to changes before this section, and at the end of each block)
-          # a1 and b1 are always the lines right before the next change.
-          if a_uniq < a1 || b_uniq < b1
-            next
-          end
-          # a0, b0 are the last agreeing lines before a change.
-          a0, a1, b0, b1 = find_changes_range(text_a, text_b, a_uniq, b_uniq, a1, b1)
-          d = add_change(d, a0, a1, b0, b1)
+      def self.diff(left, right)
+        differ = HeckelDiff.new(left,right)
+        differ.perform_diff
+      end
 
-          #set a1 and b1 to be the words after the matching uniq word
-          a1, b1 = [a_uniq + 1, b_uniq + 1]
-          a1, b1 = find_next_difference(text_a, text_b, a1, b1)
-        end
+      def initialize(left, right)
+        @left = left
+        @right = right
+      end
 
-        d
+      def perform_diff
+        unique_positions = identify_unique_postions
+        unique_positions.sort!{ |a, b| a[0] <=> b[0] }    # sort by the line in which the line was found in a
+
+        left_change_pos, right_change_pos = find_next_change
+        init_changes = ChangeData.new(left_change_pos, right_change_pos, [])
+        final_changes = unique_positions.reduce(init_changes, &method(:get_differences))
+        final_changes.change_ranges
       end
 
       private
-        def self.find_changes_range(text_a, text_b, a_uniq, b_uniq, a1, b1)
-          a0, b0 = [a1, b1]
-          # we know a_uniq to be the next line which has a corresponding b_uniq. so a1 = last line of potential change (as does b1)
-          a1, b1 = [a_uniq - 1, b_uniq - 1]
-          # loop from a1 and b1's new positions down towards a0, b0.  stop when there is a change.  This gives the bounds of the change as [a0,a1] and [b0, b1]
-          while a0 <= a1 && b0 <= b1
-            if text_a[a1] != text_b[b1]   # a change is found on lines a1 and b1.  break out of loop.
-              break
-            end
-            a1 -= 1
-            b1 -= 1
+
+        ChangeData = Struct.new(:left_change_pos, :right_change_pos, :change_ranges)
+
+        def get_differences(change_data, unique_positions)
+          left_uniq_pos, right_uniq_pos = unique_positions
+          left_pos, right_pos = change_data.left_change_pos, change_data.right_change_pos
+
+          if left_uniq_pos < left_pos || right_uniq_pos < right_pos
+            change_data
+          else
+            left_lo, left_hi, right_lo, right_hi  = find_change_ranges(left_pos, right_pos, left_uniq_pos-1, right_uniq_pos-1)
+            next_left_pos, next_right_pos = find_next_change(left_uniq_pos + 1, right_uniq_pos + 1)
+
+            updated_ranges = append_change_range(change_data.change_ranges, left_lo, left_hi, right_lo, right_hi)
+            ChangeData.new(next_left_pos, next_right_pos, updated_ranges)
           end
-          [a0, a1, b0, b1]
         end
 
-        def self.find_next_difference(text_a, text_b, a1, b1)
-          while a1 < text_a.length && b1 < text_b.length
-            if text_a[a1] != text_b[b1]
-              break
-            end
-            a1 += 1
-            b1 += 1
+        def find_change_ranges(left_lo, right_lo, left_hi, right_hi)
+          in_range = left_lo <= left_hi && right_lo <= right_hi
+          unmatched = @left[left_hi] != @right[right_hi]
+          if found_next_change(in_range, unmatched)
+            [left_lo, left_hi, right_lo, right_hi]
+          else
+            find_change_ranges(left_lo, right_lo, left_hi-1, right_hi-1)
           end
-          [a1, b1]
         end
 
-        def self.set_freq(text_a, text_b)
-          uniq = [[text_a.length, text_b.length]]
+        def find_next_change(left_pos=0, right_pos=0)
+          in_range = left_pos < @left.length && right_pos < @right.length
+          unmatched = @left[left_pos] != @right[right_pos]
+          if found_next_change(in_range, unmatched)
+            [left_pos, right_pos]
+          else
+            find_next_change(left_pos+1, right_pos+1)
+          end
+        end
+
+        def found_next_change(in_range, unmatched)
+          (in_range && unmatched) || !in_range
+        end
+
+        def identify_unique_postions
+          uniq = [[ @left.length, @right.length]]
           #start building up uniq, the set of lines which appear exactly once in each text
           freq, ap, bp = [{}, {}, {}]
-          text_a.each_with_index do |line, i|
+          @left.each_with_index do |line, i|
             freq[line] ||= 0
             freq[line] += 2                   # add 2 to the freq of line if its in text_a
             ap[line] = i                    # set ap[line] to the line number
           end
-          text_b.each_with_index do |line, i|
+          @right.each_with_index do |line, i|
             freq[line] ||= 0
             freq[line] += 3                   # add 3 to the freq of line if its in text_b
             bp[line] = i                    # set bp[line] to the line number
@@ -91,6 +101,18 @@ module Dyph3
             end
           end
           uniq
+        end
+
+        # given the calculated bounds of the 2 way diff, create the proper change type and add it to the queue.
+        def append_change_range(changes_ranges, left_lo, left_hi, right_lo, right_hi)
+          if left_lo <= left_hi && right_lo <= right_hi # for this change, the bounds are both 'normal'.  the beginning of the change is before the end.
+            changes_ranges << [:change, left_lo + 1, left_hi + 1, right_lo + 1, right_hi + 1]
+          elsif left_lo <= left_hi
+            changes_ranges << [:delete, left_lo + 1, left_hi + 1, right_lo + 1, right_lo]
+          elsif right_lo <= right_hi
+            changes_ranges << [:add, left_lo + 1, left_lo, right_lo + 1, right_hi + 1]
+          end
+          changes_ranges
         end
 
         def self.convert_to_resig_output(heckel_diff, old_text_array, new_text_array)
@@ -142,17 +164,8 @@ module Dyph3
           { old_text: old_text, new_text: new_text}
         end
 
-        # given the calculated bounds of the 2 way diff, create the proper change type and add it to the queue.
-        def self.add_change(d, a0, a1, b0, b1)
-          if a0 <= a1 && b0 <= b1 # for this change, the bounds are both 'normal'.  the beginning of the change is before the end.
-            d << [:change, a0 + 1, a1 + 1, b0 + 1, b1 + 1]
-          elsif a0 <= a1
-            d << [:delete, a0 + 1, a1 + 1, b0 + 1, b0]
-          elsif b0 <= b1
-            d << [:add, a0 + 1, a0, b0 + 1, b1 + 1]
-          end
-          d
-        end
+
+
     end
 
     class TwoWayChunk
