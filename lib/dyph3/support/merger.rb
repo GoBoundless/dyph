f2c8c7d133f1a993796c6c7c572427e9ed542f8b
Andrew Montalto <aamontal@gmail.com>
2015-12-04 16:08:07 -0500

    centralize the differs two way differ setting

--- lib/dyph3/support/merger.rb
+++ lib/dyph3/support/merger.rb
@@ -2,7 +2,7 @@ module Dyph3
   module Support
     class Merger
       attr_reader :result, :current_differ
-      def self.merge(left, base, right, current_differ: Dyph3::TwoWayDiffers::HeckelDiff)
+      def self.merge(left, base, right, current_differ: Dyph3::TwoWayDiffers::OriginalHeckelDiff)
         merger = Merger.new(left: left, base: base, right: right, current_differ: current_differ)
         merger.execute_three_way_merge
         merger.result
