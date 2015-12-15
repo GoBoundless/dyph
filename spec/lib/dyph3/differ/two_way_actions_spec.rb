87894170b68d2c9a326cac1489943d0e80791bb4
Andrew Montalto <aamontal@gmail.com>
2015-12-15 11:42:43 -0500

    remove legacy mido stuff from a test

--- spec/lib/dyph3/differ/two_way_actions_spec.rb
+++ spec/lib/dyph3/differ/two_way_actions_spec.rb
@@ -8,13 +8,7 @@ describe Dyph3::Differ do
           diff = Dyph3::Differ.merge_two_way_diff(t1, t1, diff2: diff2)
           expect(diff.map(&:class)).to eq [Dyph3::Action::NoChange, Dyph3::Action::NoChange, Dyph3::Action::NoChange, Dyph3::Action::NoChange]
         end
-        # it "should show an add" do
-        #   t1  = %w[ White Grey Pink Red Brown Orange Yellow Cyan Blue Violet Invisible ]
-        #   t2 = %w[ White Grey Pink Invisible Red Brown Orange Yellow Cyan Blue Violet ]
-        #   diff = Dyph3::Differ.merge_two_way_diff(t1, t2, diff2: diff2)
-        #   binding.pry
-        #   #expect(diff.map(&:class)).to eq [Dyph3::Action::NoChange, Dyph3::Action::NoChange, Dyph3::Action::NoChange, Dyph3::Action::NoChange, Dyph3::Action::Add]
-        # end
+       
         it "should show an add" do
           t1 = "a b c d".split
           t2 = "a b c d e".split
@@ -37,8 +31,8 @@ describe Dyph3::Differ do
         end
 
         it "should work with this real world complex example" do
-          t1 = ["#<Mido::TextBlock::Id id=\"atom_12225_brief_p_0a5374c2bb74790ed958dfb8516a76fc_0\">", "#<Mido::TextBlock::Type name=:p>", ":\"Human \"", ":\"activities \"", ":\"likely \"", ":\"caused \"", ":\"the \"", ":\"Holocene \"", ":\"mass \"", ":extinctions", ":\", \"", ":\"and \"", ":\"many \"", ":\"methods \"", ":\"have \"", ":\"been \"", ":\"employed \"", ":\"to \"", ":\"estimate \"", ":\"these \"", ":\"extinction \"", ":rates", ":\".\"", "#<Mido::TextBlock::Type closed=true name=:p>", "#<Mido::TextBlock::Id closed=true id=\"atom_12225_brief_p_0a5374c2bb74790ed958dfb8516a76fc_0\">"]
-          t2 = ["#<Mido::TextBlock::Id id=\"atom_12225_brief_p_27e015d07debd946ef199ab53ce1ca29_0\">", "#<Mido::TextBlock::Type name=:p>", ":\"Human \"", ":\"activities \"", ":\"probably \"", ":\"caused \"", ":\"the \"", ":\"Holocene \"", ":\"mass \"", ":extinctions", ":\"; \"", ":\"many \"", ":\"methods \"", ":\"have \"", ":\"been \"", ":\"employed \"", ":\"to \"", ":\"estimate \"", ":\"these \"", ":\"extinction \"", ":rates", ":\".\"", "#<Mido::TextBlock::Type closed=true name=:p>", "#<Mido::TextBlock::Id closed=true id=\"atom_12225_brief_p_27e015d07debd946ef199ab53ce1ca29_0\">"]
+          t1 = ["t1_001", "s1", "s2", "s3", "t1_002", "s4", "s5", "s6", "s7", "s8", "t1_003", "t1_004", "s9", "s10", "s11", "s12", "s13", "s14", "s15", "s16", "s17", "s18", "s19", "s20", "t1_005"]
+          t2 = ["t2_001", "s1", "s2", "s3", "t2_002", "s4", "s5", "s6", "s7", "s8", "t2_003", "s9", "s10", "s11", "s12", "s13", "s14", "s15", "s16", "s17", "s18", "s19", "s20", "t2_005"]
           diff = Dyph3::Differ.merge_two_way_diff(t1, t2, diff2: diff2)
 
           expect(diff.map(&:class)).to eq [ Dyph3::Action::Delete, Dyph3::Action::Add, Dyph3::Action::NoChange, Dyph3::Action::NoChange, Dyph3::Action::NoChange, Dyph3::Action::Delete, Dyph3::Action::Add, Dyph3::Action::NoChange, Dyph3::Action::NoChange, Dyph3::Action::NoChange, Dyph3::Action::NoChange, Dyph3::Action::NoChange, Dyph3::Action::Delete, Dyph3::Action::Delete, Dyph3::Action::Add, Dyph3::Action::NoChange, Dyph3::Action::NoChange, Dyph3::Action::NoChange, Dyph3::Action::NoChange, Dyph3::Action::NoChange, Dyph3::Action::NoChange, Dyph3::Action::NoChange, Dyph3::Action::NoChange, Dyph3::Action::NoChange, Dyph3::Action::NoChange, Dyph3::Action::NoChange, Dyph3::Action::NoChange, Dyph3::Action::Delete, Dyph3::Action::Add ]
